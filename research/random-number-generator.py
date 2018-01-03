ae6bb11fda3a372d254fdd7287670ae4c6165b1e
Matt Stein <matt.m.stein@gmail.com>
2018-01-02 22:21:24 -0800

    Got rid of the Python random functions which simplified the code.

--- research/random-number-generator.py
+++ research/random-number-generator.py
@@ -4,13 +4,13 @@ from qiskit import QuantumProgram, QISKitError
 import math, random
 # Number of qubits and classical registers
 num_qubits = 1
-shots = 1024                  # Number of times the program should run
+shots = 10000                  # Number of times the program should run
 backend = 'local_qasm_simulator'  # Whether to use the simulator or the real thing
 circuit_name = 'circuit'          # What you wish to call the circuit
 
 
 num_rand_digits = 2
-num_runs = 209
+num_runs = 1000
 digit_array = [0]*num_rand_digits
 
 # This is where the quantum and classical registers are defined
@@ -26,10 +26,13 @@ Q_SPECS = {
             'size': num_qubits
         }]}],
 }
-for _ in range(num_runs):
+
+print("Shots:", shots, " Runs: ", num_runs)
+
+for run in range(num_runs):
     try:
-        frac = 0.6 + float(random.randint(0, 9))/100.0 + float(random.randint(0, 9))/1000.0
-        frac = round(frac, 3)
+        # frac = 0.6 + float(random.randint(0, 9))/100.0 + float(random.randint(0, 9))/1000.0
+        frac = 0.6
         # Initializes the Program
         qp = QuantumProgram(specs=Q_SPECS)
         qc = qp.get_circuit(circuit_name)
@@ -52,20 +55,25 @@ for _ in range(num_runs):
         result = out.get_counts(circuit_name)
 
         random_number = (result['1']/result['0']) * (10.0 ** 18)
+        random_number = round(random_number, 0)
         string_random_number = str(format(random_number, 'f'))
 
-        for i in range(2,12):
+        for i in range(4,16):
             rand_sum = int(string_random_number[i])
-        if len(string_random_number) <= 4:
+        if len(string_random_number) <= 16:
             number = random.randint(0, num_rand_digits)
             print("Weird number, RandInt: ", number, " Frac: ", frac, "RN: ", string_random_number)
         else:
             number = rand_sum % num_rand_digits
 
+
         # The results section where you print out the information of the experimentX1
-        print("RandInt: ", number, " Frac: ", frac, "RN: ", string_random_number)
+        # print("RandInt: ", number, " Frac: ", frac, "RN: ", string_random_number)
         digit_array[int(number)] += 1
-        print(digit_array)
+
+        if run % 10 == 0:
+            print("Run: ", run)
+            print(digit_array)
 
         # For general errors, research later
     except QISKitError as ex:
